5b434adb486f243fec415f0cc5fd431450a3b306
Igor Kononenko <i.kononenko@yadro.com>
2021-08-20 12:15:34 +0300

    Initial version
    * Initialize meson build scripts
    * Supplement README.MD of build and design description.
    * Trivial implementation managering Sessions:
      + Create SessionManager
      + Create Session
      + Adjust cleanup callback
      + Remove session by uniquie ID.
    
    TODO:
    * Remove session by username, remote IPv4 address, type, unconditional.
    * Control unique session grouped by:
      + Username
      + SessionType
    
    Signed-off-by: Igor Kononenko <i.kononenko@yadro.com>

---
+++ src/session.cpp
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2021 YADRO
+
+#include <src/session.hpp>
+
+namespace obmc
+{
+namespace session
+{
+void SessionItem::delete_()
+{
+    auto manager = managerWeakPtr.lock();
+    if (manager)
+    {
+        SessionManager::SessionIdentifier sessionId =
+            SessionManager::parseSessionId(this->sessionID());
+        if (!manager->remove(sessionId))
+        {
+            throw InternalFailure();
+        }
+    }
+}
+
+void SessionItem::setSessionMetadata(std::string username,
+                                     uint32_t remoteIPAddr)
+{
+    this->adjustSessionOwner(username);
+    this->remoteIPAddr(remoteIPAddr);
+}
+
+void SessionItem::resetCleanupFn(SessionManager::SessionCleanupFn&& cleanup)
+{
+    this->cleanupFn = cleanup;
+}
+
+void SessionItem::adjustSessionOwner(const std::string& userName)
+{
+    using DBusGetObjectOut = std::map<std::string, std::vector<std::string>>;
+
+    constexpr const std::array userObjectIfaces = {
+        "xyz.openbmc_project.User.Attributes"};
+    auto userObjectPath = "/xyz/openbmc_project/user/" + userName;
+
+    DBusGetObjectOut getUserObject;
+    auto callMethod =
+        bus.new_method_call("xyz.openbmc_project.ObjectMapper",
+                            "/xyz/openbmc_project/object_mapper",
+                            "xyz.openbmc_project.ObjectMapper", "GetObject");
+    callMethod.append(userObjectPath.c_str(), userObjectIfaces);
+    bus.call(callMethod).read(getUserObject);
+
+    if (getUserObject.empty())
+    {
+        throw std::runtime_error("The username '" + userName +
+                                 "' is not found");
+    }
+
+    this->associations({
+        make_tuple("user", "session", userObjectPath),
+    });
+}
+
+} // namespace session
+} // namespace obmc
---
+++ src/session.hpp
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2021 YADRO
+
+#pragma once
+
+#include <unistd.h>
+
+#include <libobmcsession/manager.hpp>
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <xyz/openbmc_project/Object/Delete/server.hpp>
+
+namespace obmc
+{
+namespace session
+{
+
+using SessionItemServerObject =
+    sdbusplus::server::object::object<SessionItemServer>;
+using AssocDefinitionServerObject = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Association::server::Definitions>;
+
+using DeleteServerObject = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Object::server::Delete>;
+using InternalFailure =
+    sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+class SessionItem :
+    public SessionItemServerObject,
+    public AssocDefinitionServerObject,
+    public DeleteServerObject
+{
+  public:
+    SessionItem() = delete;
+    SessionItem(const SessionItem&) = delete;
+    SessionItem& operator=(const SessionItem&) = delete;
+    SessionItem(SessionItem&&) = default;
+    SessionItem& operator=(SessionItem&&) = default;
+
+    /** @brief Constructs dbus-object-server of Session Item.
+     *
+     * @param[in] bus               - Handle to system dbus
+     * @param[in] objPath           - The Dbus path that hosts Session Item.
+     * @param[in] managerWeakPtr    - The weakptr of manager.
+     */
+    SessionItem(sdbusplus::bus::bus& bus, const std::string& objPath,
+                SessionManagerWeakPtr managerWeakPtr) :
+        SessionItemServerObject(bus, objPath.c_str()),
+        AssocDefinitionServerObject(bus, objPath.c_str()),
+        DeleteServerObject(bus, objPath.c_str()), bus(bus), path(objPath),
+        managerWeakPtr(managerWeakPtr)
+    {
+        // Nothing to do here
+    }
+
+    /** @brief Constructs dbus-object-server of session item.
+     *
+     * @param[in] bus           - Handle to system dbus
+     * @param[in] objPath       - The Dbus path that hosts Session Item
+     * @param[in] cleanupFn     - The callback will be handling a customized
+     *                            cleanup of the session on the session-item
+     *                            removal.
+     */
+    SessionItem(sdbusplus::bus::bus& bus, const std::string& objPath,
+                SessionManagerWeakPtr managerWeakPtr,
+                SessionManager::SessionCleanupFn&& cleanupFn) :
+        SessionItemServerObject(bus, objPath.c_str()),
+        AssocDefinitionServerObject(bus, objPath.c_str()),
+        DeleteServerObject(bus, objPath.c_str()), bus(bus), path(objPath),
+        managerWeakPtr(managerWeakPtr), cleanupFn(cleanupFn)
+    {
+        // Nothing to do here
+    }
+
+    ~SessionItem() override
+    {
+        if (cleanupFn != nullptr)
+        {
+            std::invoke(cleanupFn, identifier);
+        }
+    }
+
+    /**
+     * @brief callback to delete object of the
+     *        `xyz.openbmc_project.Object.Delete` dbus interface method
+     *
+     */
+    void delete_() override;
+
+    /** @brief Implementation for SetSessionMetadata
+     *         Set Username and Remote IP addres of exist session.
+     *
+     *  @param[in] username         - Owner username of the session
+     *  @param[in] remoteIPAddr     - Remote IP address.
+     */
+    void setSessionMetadata(std::string username,
+                            uint32_t remoteIPAddr) override;
+
+    /**
+     * @brief Callback function to a session cleanup on the close.
+     *
+     */
+    void resetCleanupFn(SessionManager::SessionCleanupFn&&);
+
+    /**
+     * @brief Associate user of specified UID with the current session.
+     *
+     * @param userName              - the user name to associate with the
+     *                                current session.
+     *
+     * @throw std::exception          failure on set user object relation to
+     *                                the current session
+     */
+    void adjustSessionOwner(const std::string& userName);
+
+  private:
+    SessionManager::SessionIdentifier identifier;
+    sdbusplus::bus::bus& bus;
+    /** @brief Path of the group instance */
+    const std::string path;
+    SessionManagerWeakPtr managerWeakPtr;
+    SessionManager::SessionCleanupFn cleanupFn;
+};
+
+} // namespace session
+} // namespace obmc
